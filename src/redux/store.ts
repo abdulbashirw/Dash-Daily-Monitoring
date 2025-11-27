4f5629dec8f435de3a4277a17cfd506991d8364f
abdulbashir495@gmail.com <abdulbashir495@gmail.com>
2025-11-27 16:08:00 +0700

    perubahan get data card and style table bawah

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,22 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { type Action, combineReducers, configureStore } from '@reduxjs/toolkit'
 import { useDispatch, useSelector } from 'react-redux'
 import appSlice from '@src/redux/slice/app.slice.ts'
 import dailyMonitoringServiceApi from '@src/redux/service/daily-monitoring.service.ts'
 import authServiceApi from '@src/redux/service/auth.service.ts'
 import authSlice from '@src/redux/slice/auth.slice.ts'
 
+const combinedReducers = combineReducers({
+  appReducer: appSlice,
+  authReducer: authSlice,
+  [dailyMonitoringServiceApi.reducerPath]: dailyMonitoringServiceApi.reducer,
+  [authServiceApi.reducerPath]: authServiceApi.reducer,
+})
+
+export type RootState = ReturnType<typeof combinedReducers>
+
+interface ResetAction extends Action {
+  type: 'RESET'
+  [key: string]: unknown
+}
+
+const rootReducer = (state: RootState | undefined, action: ResetAction): RootState => {
+  if (action.type === 'RESET') {
+    state = undefined
+  }
+  return combinedReducers(state, action)
+}
+
 export const store = configureStore({
-  reducer: {
-    appReducer: appSlice,
-    authReducer: authSlice,
-    [dailyMonitoringServiceApi.reducerPath]: dailyMonitoringServiceApi.reducer,
-    [authServiceApi.reducerPath]: authServiceApi.reducer,
-  },
+  reducer: rootReducer,
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(dailyMonitoringServiceApi.middleware, authServiceApi.middleware),
 })
-
-export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
